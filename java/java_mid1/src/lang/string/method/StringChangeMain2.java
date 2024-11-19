0cf425a455520dcbf449acaaaf0e8975a8d4317f
JINSU <bluesea9519@gmail.com>
2024-11-19 22:56:26 +0900

    feat: 문자열 조작 및 변환2

---
+++ java/java_mid1/src/lang/string/method/StringChangeMain2.java
@@ -0,0 +1,17 @@
+package lang.string.method;
+
+public class StringChangeMain2 {
+
+    public static void main(String[] args) {
+        String strWithSpaces = "   Java Programming ";
+
+        System.out.println("소문자로 변환: " + strWithSpaces.toLowerCase());
+        System.out.println("대문자로 변환: " + strWithSpaces.toUpperCase());
+
+        System.out.println("공백 제거(trim): '" + strWithSpaces.trim() + "'");
+        System.out.println("공백 제거(strip): '" + strWithSpaces.strip() + "'"); // 유니코드 공백을 포함해서 제거
+        System.out.println("앞 공백 제거(strip) '" + strWithSpaces.stripLeading() + "'");
+        System.out.println("뒤 공백 제거(strip) '" + strWithSpaces.stripTrailing() + "'");
+
+    }
+}
