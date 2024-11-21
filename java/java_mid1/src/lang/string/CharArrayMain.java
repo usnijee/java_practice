936efb9e4e42d81521fbaf6b3f83a3676f951c0d
JINSU <bluesea9519@gmail.com>
2024-11-21 16:04:18 +0900

    feat: char와 String의 차이

---
+++ java/java_mid1/src/lang/string/CharArrayMain.java
@@ -0,0 +1,12 @@
+package lang.string;
+
+public class CharArrayMain {
+
+    public static void main(String[] args) {
+        char[] charArr = new char[]{'h','e','l','l','o'}; // char는 문자 하나를 다룰 때 사용,, char를 이용하여 여러 문자를 다루려면 char[]을 사용
+        System.out.println(charArr);
+
+        String str = "hello";
+        System.out.println("str = " + str);
+    }
+}
