b5c72cc442fad490d361aaec1955f6a382f1a038
JINSU <bluesea9519@gmail.com>
2024-11-26 16:47:02 +0900

    feat: ZoneId 시간

---
+++ java/java_mid1/src/time/ZoneIdMain.java
@@ -0,0 +1,20 @@
+package time;
+
+import java.time.ZoneId;
+
+public class ZoneIdMain {
+
+    public static void main(String[] args) {
+        for (String availableZoneId:ZoneId.getAvailableZoneIds()) {
+//            System.out.println("availableZoneId = " + availableZoneId);
+            ZoneId zoneId = ZoneId.of(availableZoneId);
+            System.out.println(zoneId + " | " + zoneId.getRules());
+        }
+
+        ZoneId zoneId = ZoneId.systemDefault();  // 시스템이 사용하는 기본 ZoneId
+        System.out.println("ZoneId.systemDefault = " + zoneId);
+
+        ZoneId seoulZoneId = ZoneId.of("Asia/Seoul"); // 타임존을 직접 제공해서 ZoneId를 반환
+        System.out.println("seoulZoneId = " + seoulZoneId);
+    }
+}
