e98c52bbb05fe27e3c4112fbadcfeeac8b61e089
JINSU <bluesea9519@gmail.com>
2024-11-28 17:24:50 +0900

    feat: 날짜 시간 조작, with() 메서드

---
+++ java/java_mid1/src/time/ChangeTimeWithMain.java
@@ -0,0 +1,33 @@
+package time;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
+
+public class ChangeTimeWithMain {
+
+    public static void main(String[] args) {
+        LocalDateTime dt = LocalDateTime.of(2018, 1, 1, 13, 30, 59);
+        System.out.println("dt = " + dt);
+
+        // Temporal with(TemporalField field, long newValue) : 날짜와 시간의 특정 필드의 값 변경 가능
+        LocalDateTime changeDt1 = dt.with(ChronoField.YEAR, 2020);
+        System.out.println("changeDt1 = " + changeDt1);
+
+        // 편의 메서드 사용
+        LocalDateTime changeDt2 = dt.withYear(2020);
+        System.out.println("changeDt2 = " + changeDt2);
+
+        //TemporalAdjuster 사용 --> 복잡한 날짜(다음주 금요일, 이번 달의 마지막 일요일) 계산 가능
+        //다음주 금요일
+        LocalDateTime with1 = dt.with(TemporalAdjusters.next(DayOfWeek.FRIDAY));
+        System.out.println("기준 날짜: " + dt);
+        System.out.println("다음 금요일: " + with1);
+
+        //이번 달의 마지막 일요일
+        LocalDateTime with2 = dt.with(TemporalAdjusters.lastInMonth(DayOfWeek.SUNDAY));
+        System.out.println("같은 달의 마지막 일요일 = " + with2);
+    }
+}
