d68b755235b7a0646adde27cfcfdde31501f61fd
JINSU <bluesea9519@gmail.com>
2024-12-03 20:59:07 +0900

    feat: 오류 사항 시뮬레이션 추가

---
+++ java/java_mid1/src/exception/ex1/NetworkClientV1.java
@@ -0,0 +1,48 @@
+package exception.ex1;
+/*
+    오류 사항 시뮬레이션
+ */
+public class NetworkClientV1 {
+
+    private final String address;
+    public boolean connectError; // 기본값 : false
+    public boolean sendError;
+
+    public NetworkClientV1(String address) {
+        this.address = address;
+    }
+
+    public String connect() {
+        // 연결 실패
+        if (connectError) {
+            System.out.println(address + " 서버 연결 실패");
+            return "connectError";
+        }
+        // 연결성공
+        System.out.println(address + " 서버 연결 성공");
+        return "success";
+    }
+
+    public String send(String data) {
+        //전송 실패
+        if (sendError) {
+            System.out.println(address + " 데이터 전송 실패");
+        }
+        //전송 성공
+        System.out.println(address + " 서버에 데이터 전송: " + data);
+        return "success";
+    }
+
+    public void disconnect() {
+        System.out.println(address + " 서버 연결 해제");
+    }
+
+    public void initError(String data) {
+        if (data.contains("error1")) {
+            connectError = true;
+        }
+        if (data.contains("error2")) {
+            sendError = true;
+        }
+    }
+}
