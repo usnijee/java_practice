f385978ebed029d8d5bd7e63b6dd90809d4c23c2
JINSU <bluesea9519@gmail.com>
2024-12-03 21:27:47 +0900

    feat: 에러가 발생해도 disconnect를 통해 서버 연결 해제

--- java/java_mid1/src/exception/ex1/NetworkServiceV1_3.java
+++ java/java_mid1/src/exception/ex1/NetworkServiceV1_3.java
@@ -7,8 +7,7 @@ public class NetworkServiceV1_3 {
         NetworkClientV1 client = new NetworkClientV1(address);
         client.initError(data); // 추가
 
-        // 결과가 성공이 아니다 == 오류다
-        // 둘중 전자, 후자중 어떤것이 더 이해하기 쉬울까
+        // 에러 발생 후 disconnect()를 통해 서버 연결 해제하도록 리팩토링
         String connectResult = client.connect();
         if (isError(connectResult)) {
             System.out.println("[네트워크 오류 발생] 오류 코드: " + connectResult); // 오류 로그 추가
