58b3befd9002f813c043cda5d6fd00e7a3f24be4
JINSU <bluesea9519@gmail.com>
2024-12-09 11:49:40 +0900

    feat: IntegerBox

---
+++ java/java_mid2/src/generic/ex1/IntegerBox.java
@@ -0,0 +1,16 @@
+package generic.ex1;
+/*
+    generic이 필요한 이유
+ */
+public class IntegerBox {
+
+    private Integer value;
+
+    public void set(Integer value) {
+        this.value = value;
+    }
+
+    public Integer get() {
+        return value;
+    }
+}
