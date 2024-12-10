02696952fe4eaf68ad500e4f13e39c8399d62e0e
JINSU <bluesea9519@gmail.com>
2024-12-10 23:47:14 +0900

    style: 불가능한 경우 추가

--- java/java_mid1/src/enumeration/ex2/StringGradeEx2_1.java
+++ java/java_mid1/src/enumeration/ex2/StringGradeEx2_1.java
@@ -8,6 +8,11 @@ public class StringGradeEx2_1 {
         int gold = discountService.discount(ClassGrade.GOLD,price);
         int diamond = discountService.discount(ClassGrade.DIAMOND,price);
 
+        // 불가능한 경우
+        //int errorcase1 = discountService.discount(2,price);
+        //int errorcase2 = discountService.discount(new ClassGrade(),price);
+        //int errorcase3 = discountService.discount(ClassGrade.VIP,price);
+
         System.out.println("BASIC 등급의 할인 금액: " + basic);
         System.out.println("GOLD 등급의 할인 금액: " + gold);
         System.out.println("DIAMOND 등급의 할인 금액: " + diamond);
