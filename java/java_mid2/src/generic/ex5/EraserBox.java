9043c6c77657eced3e6271f63fa9c534acba4e1c
JINSU <bluesea9519@gmail.com>
2024-12-10 12:41:05 +0900

    feat: 타입 이레이저

---
+++ java/java_mid2/src/generic/ex5/EraserBox.java
@@ -0,0 +1,12 @@
+package generic.ex5;
+
+public class EraserBox<T> {
+
+    public boolean instanceCheck(Object param) {
+        return param instanceof T;
+    }
+
+    public void create() {
+//        return new T(); // 오류
+    }
+}
