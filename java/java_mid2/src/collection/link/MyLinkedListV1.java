033a4de3fc248eb0fa4df5c533c18ce326fef96f
JINSU LEE <bluesea9519@gmail.com>
2024-12-16 13:29:41 +0900

    feat: linkedlist 기본 기능 구현

---
+++ java/java_mid2/src/collection/link/MyLinkedListV1.java
@@ -0,0 +1,73 @@
+package collection.link;
+public class MyLinkedListV1 {
+
+    private Node first;
+    private int size = 0;
+
+    // add : 마지막에 데이터 추가
+    public void add(Object e) {
+        Node newNode = new Node(e);
+        if (first == null) {
+            first = newNode;
+        } else {
+            Node lastNode = getLastNode();
+            lastNode.next = newNode;
+        }
+        size++;
+
+    }
+
+    private Node getLastNode() {
+        Node x = first;
+        while (x.next != null) {
+            x = x.next;
+        }
+        return x;
+    }
+
+    // 특정 index의 값 변경
+    public Object set(int index, Object element) {
+        Node x = getNode(index);
+        Object oldValue = x.item;
+        x.item = element;
+        return oldValue;
+    }
+
+    // 특정 index의 value값 조회
+    public Object get(int index) {
+        Node node = getNode(index);
+        return node.item;
+    }
+
+    // 특정 index의 노드 조회 메서드
+    private Node getNode(int index) {
+        Node x = first;
+        for (int i = 0; i < index; i++) {
+            x = x.next;
+        }
+        return x;
+    }
+
+    public int indexOf(Object o) {
+        int index = 0;
+        for (Node x = first; x != null; x = x.next) {
+            if (o.equals(x.item)) {
+                return index;
+            }
+            index++;
+        }
+        return -1;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return "MyLinkedListV1{" +
+                "first=" + first +
+                ", size=" + size +
+                '}';
+    }
+}
