52ac299023a1b67b0bcde0303ecbe350308da64e
JINSU LEE <bluesea9519@gmail.com>
2024-12-16 14:02:30 +0900

    feat: linkedlist 제너릭 활용

---
+++ java/java_mid2/src/collection/link/MyLinkedListV3Main.java
@@ -0,0 +1,21 @@
+package collection.link;
+
+public class MyLinkedListV3Main {
+
+    public static void main(String[] args) {
+        MyLinkedListV3<String> stringList = new MyLinkedListV3<>();
+        stringList.add("a");
+        stringList.add("b");
+        stringList.add("c");
+        System.out.println(stringList);
+        String string = stringList.get(0);
+        System.out.println("string = " + string);
+
+        MyLinkedListV3<Integer> intList = new MyLinkedListV3<>();
+        intList.add(1);
+        intList.add(2);
+        intList.add(3);
+        System.out.println(intList);
+
+    }
+}
