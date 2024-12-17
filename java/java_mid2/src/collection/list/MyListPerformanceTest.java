f0379691fcdbf4eef8feb3bc52a8684e14808fe7
JINSU <bluesea9519@gmail.com>
2024-12-17 15:30:23 +0900

    feat: MyArrayList, MyLinkedList 성능 비교

---
+++ java/java_mid2/src/collection/list/MyListPerformanceTest.java
@@ -0,0 +1,90 @@
+package collection.list;
+
+public class MyListPerformanceTest {
+
+    public static void main(String[] args) {
+        int size = 50_000;
+        System.out.println("==MyArrayList 추가==");
+        addFirst(new MyArrayList<>(), size);
+        addMid(new MyArrayList<>(), size); // 위치 찾기 O(1), 데이터 추가 O(n)
+
+        MyArrayList<Integer> arrayList = new MyArrayList<>(); // 검색 성능을 평가하기위해 새로운 객체를 만들어 데이터 넣어 두기 위함
+        addLast(arrayList, size); // 위치 찾기 O(1), 데이터 추가 O(1)
+
+        int loop = 10000;
+        System.out.println("==MyArrayList 조회==");
+        getIndex(arrayList, loop, 0);
+        getIndex(arrayList, loop, size / 2);
+        getIndex(arrayList, loop, size - 1);
+
+        System.out.println("==MyArrayList 검색==");
+        search(arrayList, loop, 0);
+        search(arrayList, loop, size / 2);
+        search(arrayList, loop, size - 1);
+
+        System.out.println("==MyLinkedList 추가==");
+        addFirst(new MyLinkedList<>(), size);
+        addMid(new MyLinkedList<>(), size); // 위치 찾기 O(n), 데이터 추가 O(1)
+
+        MyLinkedList<Integer> linkedList = new MyLinkedList<>();
+        addLast(linkedList, size); // 위치 찾기 O(n), 데이터 추가 O(1)
+
+        System.out.println("==MyLinkedList 조회==");
+        getIndex(linkedList, loop, 0);
+        getIndex(linkedList, loop, size / 2);
+        getIndex(linkedList, loop, size - 1);
+
+        System.out.println("==MyLinkedList 검색==");
+        search(linkedList, loop, 0);
+        search(linkedList, loop, size / 2);
+        search(linkedList, loop, size - 1);
+
+
+    }
+
+    // MyList를 매개변수 -> 의존주입 -> 재사용성 높임
+    private static void addFirst(MyList<Integer> list, int size) {
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < size; i++) {
+            list.add(0, i);
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("앞에 추가 - 크기: " + size + ", 계산 시간: " + (endTime - startTime) + "ms");
+    }
+
+    private static void addMid(MyList<Integer> list, int size) {
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < size; i++) {
+            list.add(i / 2, i);
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("중간에 추가 - 크기: " + size + ", 계산 시간: " + (endTime - startTime) + "ms");
+    }
+
+    private static void addLast(MyList<Integer> list, int size) {
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < size; i++) {
+            list.add(i);
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("뒤에 추가 - 크기: " + size + ", 계산 시간: " + (endTime - startTime) + "ms");
+    }
+
+    private static void getIndex(MyList<Integer> list, int loop, int index) {
+        long stratTime = System.currentTimeMillis();
+        for (int i = 0; i < loop; i++) {
+            list.get(index);
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("index: " + index + ", 반복: " + loop + ", 계산 시간: " + (endTime - stratTime) + "ms");
+    }
+
+    private static void search(MyList<Integer> list, int loop, int findValue) {
+        long stratTime = System.currentTimeMillis();
+        for (int i = 0; i < loop; i++) {
+            list.indexOf(findValue);
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("findValue: " + findValue + ", 반복: " + loop + ", 계산 시간: " + (endTime - stratTime) + "ms");
+    }
+}
