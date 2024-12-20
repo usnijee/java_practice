95cf984fb16039dc99cf550dedd85b63f56b74e2
JINSU LEE <bluesea9519@gmail.com>
2024-12-20 12:44:45 +0900

    feat: 해시 자료구조 사용을 위한 hashCode() 재정의

--- java/java_mid2/src/collection/set/member/Member.java
+++ java/java_mid2/src/collection/set/member/Member.java
@@ -1,4 +1,39 @@
 package collection.set.member;
+/**
+ *  자바에서 제공하는 hashCode() : 모든 객체 타입에 대해 hash코드를 제공
+ */
+
+import java.util.Objects;
 
 public class Member {
+
+    private String id;
+
+    public Member(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        return Objects.equals(id, member.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id='" + id + '\'' +
+                '}';
+    }
 }
