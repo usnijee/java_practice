8345dbe5ef2cce883feab0ce5c27993ca40c1a90
JINSU <bluesea9519@gmail.com>
2024-12-23 19:28:16 +0900

    refactor: Map 문제3

---
+++ java/java_mid2/src/collection/map/test/WordFrequencyTest1.java
@@ -0,0 +1,28 @@
+package collection.map.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 각각의 단어가 나타난 수를 출력해라.
+ * 실행 결과를 참고하자
+ */
+public class WordFrequencyTest1 {
+
+    public static void main(String[] args) {
+        String text = "orange banana apple apple banana apple";
+        String[] texts = text.split(" ");
+
+        Map<String, Integer> map = new HashMap<>();
+        for (String t : texts) {
+            Integer count = map.get(t);
+            if (count == null) {
+                count = 0;
+            }
+            count++;
+            map.put(t, count);
+        }
+        System.out.println(map);
+
+    }
+}
