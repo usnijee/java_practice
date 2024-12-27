c3f98e183db222e88cfffa86e78da2fd8a46fdfb
JINSU <bluesea9519@gmail.com>
2024-12-28 01:58:59 +0900

    feat: MyLinkedListMain

---
+++ java/java_mid2/src/review/collection/link/Re1/MyLinkedListMain.java
@@ -0,0 +1,14 @@
+package review.collection.link.Re1;
+
+public class MyLinkedListMain {
+
+    public static void main(String[] args) {
+
+        MyLinkedList linkedList = new MyLinkedList();
+        linkedList.add(0, "a");
+        linkedList.add("b");
+        linkedList.add(0,"ff");
+        System.out.println(linkedList);
+
+    }
+}
