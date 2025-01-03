869056b5b982d6de5105b71bef064219c7c2cabb
JINSU <bluesea9519@gmail.com>
2025-01-03 12:21:30 +0900

    feat: HashMap, LinkedHashMap, TreeMap 복습

---
+++ java/java_mid2/src/review/collection/map/JavaMapMain.java
@@ -0,0 +1,34 @@
+package review.collection.map;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class JavaMapMain {
+
+    public static void main(String[] args) {
+        run(new HashMap<>());
+        run(new LinkedHashMap<>());
+        run(new TreeMap<>());
+    }
+
+    private static void run(Map<String, Integer> map) {
+        System.out.println("map = " + map.getClass());
+        map.put("C", 10);
+        map.put("B", 20);
+        map.put("A", 30);
+        map.put("1", 10);
+        map.put("2", 10);
+
+        Set<String> keySet = map.keySet();
+        Iterator<String> iterator = keySet.iterator();
+        while (iterator.hasNext()) {
+            String key = iterator.next();
+            System.out.println("key = " + key + ", value = " + map.get(key));
+        }
+        System.out.println();
+    }
+}
