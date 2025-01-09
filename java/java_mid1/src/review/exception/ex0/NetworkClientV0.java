e2a87e818dedcfddae41ad39ad4335a702cd83ab
JINSU LEE <bluesea9519@gmail.com>
2025-01-09 16:03:25 +0900

    feat: 외부 접속, 데이터 전송, 외부 접속 종료 역할

---
+++ java/java_mid1/src/review/exception/ex0/NetworkClientV0.java
@@ -0,0 +1,28 @@
+package review.exception.ex0;
+
+public class NetworkClientV0 {
+
+    private final String address; // 연결할 외부 서버 주소
+
+    public NetworkClientV0(String address) {
+        this.address = address;
+    }
+
+    public String connect() {
+        // 연결 성공
+        System.out.println(address + " 서버 연결 성공");
+        return "success";
+    }
+
+    public String send(String data) {
+        // 전송 성공
+        System.out.println(address + " 서버에 데이터 전송: " + data);
+        return "success";
+    }
+
+    public void disconnect() {
+        System.out.println(address + " 서버 연결 해제");
+    }
+
+
+}
