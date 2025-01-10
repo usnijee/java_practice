eb11015f0c3fe869cb0ad0200192b66fcaa9ba47
LEE JINSU <bluesea9519@gmail.com>
2025-01-10 21:49:03 +0900

    feat: NetworkClientException 예외, ConnectionException, SendException 예외의 부모 예외

---
+++ java/java_mid1/src/review/exception/ex3/exception/NetworkClientExceptionV3.java
@@ -0,0 +1,7 @@
+package review.exception.ex3.exception;
+
+public class NetworkClientExceptionV3 extends Exception{
+    public NetworkClientExceptionV3(String message) {
+        super(message);
+    }
+}
