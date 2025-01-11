c5c8699977c9763c596a2492a031f1bc6d8307c7
JINSU <bluesea9519@gmail.com>
2025-01-11 14:11:01 +0900

    refactor: getRandomNums를 통해 당첨 번호 로직과 개인의 번호 로직에 공통으로 사용하도록 메서드 추출

--- java/java_zerobase/src/miniTest/mini1/Assign7.java
+++ java/java_zerobase/src/miniTest/mini1/Assign7.java
@@ -14,6 +14,14 @@ public class Assign7 {
 
         System.out.print("로또 개수를 입력해 주세요.(숫자 1 ~ 10):");
         int quantity = scanner.nextInt();
+        displayMyLotto(quantity, count, random);
+
+        System.out.println("[로또 발표]");
+        Set<Integer> winningNumbers = makeWinningNumbers(count, random);
+
+    }
+
+    private static void displayMyLotto(int quantity, int count, Random random) {
         int order = 0;
         List<Set<Integer>> myLotto = makeLotto(quantity, count, random);
         for (Set<Integer> lottoEach : myLotto) {
@@ -25,34 +33,34 @@ public class Assign7 {
                 }
                 result.append(eachNum);
             }
-            System.out.printf("%-3c %s \n",alphabet,result.toString());
+            System.out.printf("%-3c %s \n", alphabet, result.toString());
+            System.out.println();
             order++;
         }
-
-        Set<Integer> winningNumbers = makeWinningNumbers(count, random);
-
     }
 
     // quantity만큼의 로또 라인 생성 함수
     private static List<Set<Integer>> makeLotto(int quantity, int count, Random random) {
-        List<Set<Integer>> lottoPaper = new ArrayList<>();
+        List<Set<Integer>> lottoPaper = new ArrayList<>(); // 여러 로또 라인을 하나의 로또 종이안에 구현하기 위함
 
-        for (int i = 0; i < quantity; i++) { // quantity만큼 로또 줄 생성
-            Set<Integer> lottoEachLine = new HashSet<>();
-            while (lottoEachLine.size() < count) {
-                int num = random.nextInt(1, 46);
-                lottoEachLine.add(num); // 로또 번호 생성
-            }
+        for (int i = 0; i < quantity; i++) {
+            Set<Integer> lottoEachLine = getRandomNums(count, random);
             lottoPaper.add(lottoEachLine);
         }
         return lottoPaper;
     }
+
     private static Set<Integer> makeWinningNumbers(int count, Random random) {
-        Set<Integer> winningNums = new HashSet<>();
-        while (winningNums.size() < count) {
-            int num = random.nextInt(1, 46);
-            winningNums.add(num);
-        }
+        Set<Integer> winningNums = getRandomNums(count, random);
         return winningNums;
+    }
+
+    private static Set<Integer> getRandomNums(int count, Random random) {
+        Set<Integer> lottoEachLine = new HashSet<>(); // 로또 번호의 중복을 방지하기 위해 Set 사용
+        while (lottoEachLine.size() < count) {
+            int num = random.nextInt(1, 46);
+            lottoEachLine.add(num); // 로또 번호 생성
+        }
+        return lottoEachLine;
     }
 }
