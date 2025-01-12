a2c56ec89ce931bc3e5630e1c6648ebc9267d9fe
JINSU <bluesea9519@gmail.com>
2025-01-12 23:51:44 +0900

    feat: 스트림 예제

---
+++ java/java_zerobase/src/stream/Practice.java
@@ -0,0 +1,27 @@
+package stream;
+// Practice
+// 하기 예제를 스트림으로 구현해보세요.
+
+import java.util.stream.IntStream;
+
+public class Practice {
+    public static void main(String[] args) {
+
+        // 예제: 1~10 숫자 중 짝수 들의 합
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int sum = 0;
+
+        for (int num: arr) {
+            if (num % 2 == 0) {
+                sum += num;
+            }
+        }
+        System.out.println("sum = " + sum);
+
+        // 스트림으로 구현
+        IntStream intStream = IntStream.rangeClosed(1, 10).filter(n -> n % 2 == 0);
+        System.out.println("sum = " + intStream.reduce((x, y) -> x + y).getAsInt());
+
+
+    }
+}
