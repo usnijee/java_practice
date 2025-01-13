86d371e9ca0b6167124bea56ae22de35d4a69467
JINSU <bluesea9519@gmail.com>
2025-01-13 21:06:16 +0900

    feat: Assign8 임시 업로드

---
+++ java/java_zerobase/src/miniTest/mini1/assign8/Assign8.java
@@ -0,0 +1,16 @@
+package miniTest.mini1.assign8;
+
+import java.util.Scanner;
+
+public class Assign8 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("[과세금액 계산 프로그램]");
+        System.out.print("연소득을 입력해 주세요.:");
+        int annualIncome = scanner.nextInt();
+        CITRate.calculateTax(annualIncome);
+
+    }
+}
