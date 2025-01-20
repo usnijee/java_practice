b90474af61173600e9f8e1462deef6f6b5942507
JINSU <bluesea9519@gmail.com>
2025-01-20 21:37:17 +0900

    feat: Practice3

---
+++ java/java_zerobase/src/dataStructure/array/practice/Practice3.java
@@ -0,0 +1,25 @@
+package dataStructure.array.practice;
+
+import java.util.Arrays;
+
+public class Practice3 {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 3, 5, 7, 9};
+//        int[] result = new int[arr.length];
+//
+//        for (int i = arr.length - 1; i >= 0 ; i--) {
+//            result[arr.length - 1 - i] = arr[i];
+//        }
+//
+//        System.out.println(Arrays.toString(result));
+//    }
+        for (int i = 0; i < arr.length / 2; i++) {
+            int tmp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = tmp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
