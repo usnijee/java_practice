f56063df6d554f78218758144bbe0bfbe2d49f74
JINSU <bluesea9519@gmail.com>
2025-01-27 08:49:26 +0900

    refactor: 이차원 배열에서의 이진탐색 접근법 기록

--- java/java_zerobase/src/algorithm/binarySearch/practice/Practice3_2.java
+++ java/java_zerobase/src/algorithm/binarySearch/practice/Practice3_2.java
@@ -1,5 +1,18 @@
 package algorithm.binarySearch.practice;
 
+/**
+ * 이차원 배열을 matrix[mid / cols][mid % cols]게 인덱싱한 이유?
+ *
+ * -> 이차원 배열을 1차원 배열처럼 인덱싱하기 위함 ex. matrix는 이차원 배열이고
+ * matrix의 11은 [1][1]의 인덱스를 가짐 -> matrix가 일차원 배열이었다면 11은 인덱스 몇이었을까?
+ *
+ * 1, 3, 7, 8, 10, 11, 15, 20 ,21, 30, 35 60 순으로 1차원 배열이었을것이므로 11은 인덱스가 5였을것이다.
+ *
+ * matrix[mid / cols][mid % cols]에 그대로 적용해보자
+ *
+ * matrix[5/4][5/4] = matrix[1][1]이므로 mid가 5일때 11임을 알 수 있다. 이러한 방식으로 모든 이차원 배열의 요소들을 일차원 배열로 표현이 가능하며
+ * 해당 mid를 통해 이진탐색이 가능하다
+ */
 public class Practice3_2 {
 
     public static void main(String[] args) {
