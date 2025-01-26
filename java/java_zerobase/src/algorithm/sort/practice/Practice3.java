5d1a67fad9776c8fbc9f55df4758f3f718d2462d
JINSU LEE <bluesea9519@gmail.com>
2025-01-26 18:03:50 +0900

    feat: Practice3

--- java/java_zerobase/src/algorithm/sort/practice/Practice3.java
+++ java/java_zerobase/src/algorithm/sort/practice/Practice3.java
@@ -14,12 +14,79 @@ import java.util.Arrays;
 public class Practice3 {
 
     public static ArrayList<int[]> solution(int[][] intervals) {
+        if (intervals == null || intervals.length < 2) {
+            return new ArrayList<>();
+        }
 
-        return null;
+        sort(intervals);
+
+        ArrayList<int[]> result = new ArrayList<>();
+        int[] curInterval = intervals[0]; // 구간의 첫번째 인터벌
+        result.add(curInterval);
+
+        for (int i = 1; i < intervals.length; i++) {
+            if (curInterval[1] >= intervals[i][0]) {
+                /**
+                 * curInterval의 열값이 그 다음 intervals의 행보다 크면
+                 * 두 구간은 무조건 겹치는 구간이 있다는 것을 의미
+                 */
+                curInterval[1] = intervals[i][1];
+            } else {
+                /**
+                 * 겹치는 구간이 없다면 curInterval을 그 다음으로 갱신
+                 */
+                curInterval = intervals[i];
+                result.add(curInterval);
+            }
+        }
+        return result;
     }
 
     public static void sort(int[][] intervals) {
+        /**
+         * quickSort를 활용하여 2차원배열의 행을 기준으로 2차원 배열을 정렬
+         * ex. 정렬된 결과
+         * {{1, 3}, {2, 6}, {8, 10], {15, 18}}
+         */
+        quickSort(intervals, 0, intervals.length - 1);
+    }
 
+    public static void quickSort(int[][] arr, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+
+        int pivot = partition(arr, start, end);
+
+        quickSort(arr, start, pivot - 1);
+        quickSort(arr, pivot + 1, end);
+    }
+
+    private static int partition(int[][] arr, int start, int end) {
+        int pivot = arr[start][0];
+        int i = start;
+        int j = end;
+
+        while (i < j) {
+            while (arr[j][0] > pivot && i < j) {
+                j--;
+            }
+
+            while (arr[i][0] <= pivot && i < j) {
+                i++;
+            }
+
+            swap(arr, i, j);
+        }
+        swap(arr, start, i);
+
+        return i;
+    }
+
+    private static void swap(int[][] arr, int i, int j) {
+        int[] tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
 
     public static void main(String[] args) {
