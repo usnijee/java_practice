0291cd8d5b9adcd17580f008274abe23cccf326d
JINSU LEE <bluesea9519@gmail.com>
2025-01-30 20:22:08 +0900

    feat: temporary upload

---
+++ java/java_zerobase/src/dataStructure/linear/practice/practice3/Practice3.java
@@ -0,0 +1,37 @@
+package dataStructure.linear.practice.practice3;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class Practice3 {
+    public static void solution(int[] data) {
+        int[] balloon = new int[data.length];
+
+        for (int i = 0; i < data.length; i++) {
+            balloon[i] = i + 1;
+        }
+
+        int p1 = 0;
+        int start = 0;
+
+        for (int i = 0; i < data.length; i++) {
+            if (i == 0) {
+
+            }
+            if (data[i] > 0) {
+                start = i + data[i];
+            }
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        int[] balloon = {3, 2, 1, -3, -1};
+        solution(balloon);
+
+        System.out.println();
+        balloon = new int[]{3, 2, -1, -2};
+        solution(balloon);
+    }
+}
