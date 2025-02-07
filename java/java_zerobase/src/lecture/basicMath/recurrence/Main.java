8324fa30559c7a166ee2523e8ae6da552ba0bd7b
JINSU LEE <bluesea9519@gmail.com>
2025-02-07 18:27:46 +0900

    feat: 점화식과 재귀함수를 활용하기 (등차, 등비, 피보나치)

--- java/java_zerobase/src/lecture/basicMath/recurrence/Main.java
+++ java/java_zerobase/src/lecture/basicMath/recurrence/Main.java
@@ -1,6 +1,30 @@
-package lecture.basicMath.recursion;// 기초 수학 - 점화식과 재귀함수
+package lecture.basicMath.recurrence;// 기초 수학 - 점화식과 재귀함수
 
 public class Main {
+
+    static int recursion1(int n) {
+        if (n == 1) {
+            /*
+            재귀함수 종료 조건
+             */
+            return 1;
+        }
+        return 3 * recursion1(n - 1);
+    }
+
+    static int recursion2(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return recursion2(n - 1) + 1;
+    }
+
+    static int recursion3(int n) {
+        if (n < 3) {
+            return 1;
+        }
+        return recursion3(n - 2) * recursion3(n - 1);
+    }
 
     public static void main(String[] args) {
         
@@ -33,6 +57,19 @@ public class Main {
 //      1, 1, 2, 3, 5, 8, 13, ...의 n번 째 수
         n = 6;
         result = 0;
+        int a1 = 1;
+        int a2 = 1;
+
+        if (n < 3) {
+            result = 1;
+        } else {
+            for (int i = 2; i < n; i++) {
+                result = a1 + a2;
+                a1 = a2;
+                a2 = result;
+            }
+        }
+        System.out.println(result);
 
 
     }
